66f26f1bebc1827eb17eb98daa1c34283ad30cd9
hryankim-architect <hryankim1221@gmail.com>
2026-05-24 09:27:55 -0700

    Initial commit

---
+++ src/bioscaffold/__init__.py
@@ -0,0 +1,9 @@
+"""bioscaffold: capability-portrait scaffold for bioinformatics R&D repos.
+
+Rename this package to your project's name when you create a new repo from
+this template. The substrate hooks in :mod:`bioscaffold.audit`,
+:mod:`bioscaffold.tracking`, and :mod:`bioscaffold.canary` are designed to
+be copy-and-edit, not pip-installed.
+"""
+
+__version__ = "0.1.0"
